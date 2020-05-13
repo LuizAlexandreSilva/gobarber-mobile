4bf57519680f63716fa62bd27eb55654a49ecf43
Luiz Alexandre Silva <alexandrehsilva96@gmail.com>
2020-05-12 20:26:07 -0400

    structure & patterns

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Container } from './styles';
+
+import logoImg from '../../assets/logo.png';
+
+const SignUp: React.FC = () => {
+  return <Container />;
+};
+
+export default SignUp;
